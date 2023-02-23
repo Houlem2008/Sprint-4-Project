5ae97ce84bd186bda9eccedfdb46d454603d2fae
Houlem2008 <124086219+Houlem2008@users.noreply.github.com>
2023-02-23 04:07:49 -0600

    App.py updated.

--- app.py
+++ app.py
@@ -1,67 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[30]:
-
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
-# #### What we need to include in the app: ####
-# - at least one `st.header` with text
-# - at least one `plotly-express` histogram using `[st.write][https://docs.streamlit.io/library/api-reference/write-magic/st.write][st.plotly_chart](https://docs.streamlit.io/library/api-reference/charts/st.plotly_chart)`
-# - at least one `plotly-express` scatterplot using `[st.write][https://docs.streamlit.io/library/api-reference/write-magic/st.write][st.plotly_chart](https://docs.streamlit.io/library/api-reference/charts/st.plotly_chart)`
-# - at least one checkbox that uses `[st.checkbox](https://docs.streamlit.io.library/api-reference/widgets/st.checkbox)` that changes the behavior of any of the above components
-
-# In[31]:
-
-
 df = pd.read_csv('moved_vehicles_us.csv')
 
-
-# ### Overview of data
-
-# In[32]:
-
+# Overview of data
 
 df.info()
 
-
-# In[33]:
-
-
 df.head(10)
 
-
-# ### Preprocessing data
-
-# In[34]:
-
+# Preprocessing data
 
 df['date_posted'] = pd.to_datetime(df['date_posted'], format='%Y-%m-%d') # Converting dates to datetime data type
 df.isna().sum() # Getting a count of missing values by column
 
-
 # The `is_4wd` column appears to contain the value of `1.0` if the vehicle was listed to have four wheel drive, and a missing value if not. This will be confirmed to get the unique values, including missing from this column, and if there are only the two aforementioned values, we will replace missing values with `0` (zero) and convert them to integers to act as a Bulian true or false, 1 = true, 0 = false. This doesn't mean that all the vehicles listed with a 0 (zero) do not have four wheel drive, but that they were not advertised to have it.
 
-# In[35]:
-
-
 df['is_4wd'].value_counts(dropna=False)
-
-
-# In[36]:
-
 
 df['is_4wd'] = df['is_4wd'].fillna(0)
 df = df.astype({'is_4wd':'int64'})
 df['is_4wd'].value_counts(dropna=False)
-
-
-# In[37]:
-
 
 makemodel = df["model"].str.split(" ", n = 1, expand = True)
 df['make'] = makemodel[0]
@@ -69,11 +32,7 @@ df['model'] = makemodel[1]
 df.rename(columns={'model_year':'year', 'paint_color':'color'}, inplace=True)
 df = df[['price', 'year', 'make', 'model', 'condition', 'cylinders', 'fuel','odometer', 'transmission', 'type', 'color', 'is_4wd', 'date_posted', 'days_listed']]
 
-
-# ### Streamlit coding
-
-# In[38]:
-
+# Streamlit coding
 
 st.header('Exploratory Analysis of Auto Listings')
 st.write('''
@@ -81,27 +40,16 @@ Default display of vehicles only includes those listed for 30 days or less. In o
 ''')
 old_listings = st.checkbox('Show cars listed over 30 days')
 
-
-# In[39]:
-
-
 # Checkbox to include vehicles posted for over 30 days
 if not old_listings:
     df = df[df.days_listed<=30]
 
 
-# ##### histogram for price(y) based on year(x) (slider for year)
-
-# In[40]:
-
+# histogram for price(y) based on year(x) (slider for year)
 
 # Selectbox for histogram
 hist_list = ['condition', 'fuel', 'transmission', 'type', 'is_4wd']
 hist_select = st.selectbox('Filter for average price', hist_list)
-
-
-# In[41]:
-
 
 # Slider for year, limits then slider
 min_year, max_year = int(df['year'].min()), int(df['year'].max())
@@ -110,16 +58,8 @@ year_range = st.slider(
     'Choose years',
     value=(min_year, max_year), min_value=min_year, max_value=max_year )
 
-
-# In[42]:
-
-
 year_act_range = list(range(year_range[0],year_range[1]+1))
-year_df = df[(df.type == type_choice) & (df.year.isin(list(year_act_range)))]
-
-
-# In[43]:
-
+year_df = df[(df.year.isin(list(year_act_range)))]
 
 fig1 = px.histogram(year_df, x='year', y='price', histfunc='avg', color=hist_select)
 
@@ -127,25 +67,13 @@ fig1.update_layout(title='<b>Average Price by Year</b>')
 
 st.plotly_chart(fig1)
 
-
-# In[44]:
-
-
 fig1.show()
 
-
-# ##### scatterplot for odometer(y) based on price(x) (slider for price)
-
-# In[45]:
-
+# scatterplot for odometer(y) based on price(x) (slider for price)
 
 # Selectbox for scatterplot
 scat_list = ['condition', 'fuel', 'transmission', 'type', 'is_4wd']
 scat_select = st.selectbox('Filter for mileage by price', scat_list)
-
-
-# In[46]:
-
 
 # Slider for price, limits then slider
 min_price, max_price = int(df['price'].min()), int(df['price'].max())
@@ -154,16 +82,8 @@ price_range = st.slider(
     'Set price range',
     value=(min_price, max_price), min_value=min_price, max_value=max_price )
 
-
-# In[47]:
-
-
 price_act_range = list(range(price_range[0],price_range[1]+1))
-price_df = df[(df.type == type_choice) & (df.price.isin(list(price_act_range)))]
-
-
-# In[48]:
-
+price_df = df[(df.price.isin(list(price_act_range)))]
 
 fig2 = px.scatter(price_df, x='price', y='odometer', color=scat_select)
 
@@ -171,25 +91,13 @@ fig2.update_layout(title='<b>Mileage by Price (USD)</b>')
 
 st.plotly_chart(fig2)
 
-
-# In[49]:
-
-
 fig2.show()
 
-
-# ##### boxplot for odometer (y) based on cylinders (slider of cylinders)
-
-# In[50]:
-
+# boxplot for odometer (y) based on cylinders (slider of cylinders)
 
 # Selectbox for box chart
 box_list = ['condition', 'fuel', 'transmission', 'type', 'is_4wd']
 box_select = st.selectbox('Filter for mileage by cylinder count', box_list)
-
-
-# In[51]:
-
 
 # Slider of cylinders, limits then slider
 min_cyl, max_cyl = int(df['cylinders'].min()), int(df['cylinders'].max())
@@ -198,16 +106,8 @@ cyl_range = st.slider(
     'Specify number of cylinders',
     value=(min_cyl, max_cyl), min_value=min_cyl, max_value=max_cyl )
 
-
-# In[52]:
-
-
 cyl_act_range = list(range(cyl_range[0],cyl_range[1]+1))
-cyl_df = df[(df.type == type_choice) & (df.cylinders.isin(cyl_act_range))]
-
-
-# In[53]:
-
+cyl_df = df[(df.cylinders.isin(cyl_act_range))]
 
 fig3 = px.box(cyl_df, x='cylinders', y='odometer', color=box_select)
 
@@ -215,15 +115,4 @@ fig3.update_layout(title='<b>Distribution of Mileage by Cylinder Count</b>')
 
 st.plotly_chart(fig3)
 
-
-# In[54]:
-
-
 fig3.show()
-
-
-# In[ ]:
-
-
-
-
